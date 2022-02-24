30d8ce6ef2e9ec7de2bf391c8992d659bde47bbf
Atanu-Sabyasachi <jena.atanusabyasachi@gmail.com>
2022-02-25 00:56:32 +0530

    audio player implimented

--- lib/music_player.dart
+++ lib/music_player.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +12,11 @@ class MusicPlayer extends StatefulWidget {
 
 class _MusicPlayerState extends State<MusicPlayer> {
   //final double screenHeight = MediaQuery.of(context).size.height;
+  Duration _duration = new Duration();
+  Duration _position = new Duration();
+
+  bool isRepeat = false;
+  Color color = Colors.green;
 
   AudioPlayer? audioPlayer = AudioPlayer();
   PlayerState audioPlayerState = PlayerState.PAUSED;
@@ -19,6 +26,22 @@ class _MusicPlayerState extends State<MusicPlayer> {
   @override
   void initState() {
     super.initState();
+
+    //max duration of mp3 file
+    audioPlayer?.onDurationChanged.listen(
+      (Duration d) {
+        //print('Max duration: $d');
+        setState(() => _duration = d);
+      },
+    );
+
+    //current playtime of mp3 file
+    audioPlayer?.onAudioPositionChanged.listen(
+      (Duration p) {
+        //print('Current position: ${p.inSeconds}');
+        setState(() => _position = p);
+      },
+    );
 
     audioCache = AudioCache(fixedPlayer: audioPlayer);
     audioPlayer?.onPlayerStateChanged.listen(
@@ -31,7 +54,109 @@ class _MusicPlayerState extends State<MusicPlayer> {
       },
     );
 
-    //advancedPlayer = AudioPlayer();
+    //after complete the song the slider will be back to start
+    audioPlayer?.onPlayerCompletion.listen(
+      (event) {
+        setState(() {
+          _position = Duration(seconds: 0);
+          //buttonRepeat();
+        });
+      },
+    );
+  }
+
+  //slider implimentation
+  Widget slider() {
+    return Slider(
+      activeColor: Colors.green,
+      inactiveColor: Colors.grey,
+      value: _position.inSeconds.toDouble(),
+      min: 0.0,
+      max: _duration.inSeconds.toDouble(),
+      onChanged: (double value) {
+        setState(() {
+          changeToSecond(value.toInt());
+          value = value;
+        });
+      },
+    );
+  }
+
+  //slider in seconds
+  void changeToSecond(int second) {
+    Duration newDuration = Duration(seconds: second);
+    audioPlayer?.seek(newDuration);
+  }
+
+  //fastforward button
+  Widget buttonFast() {
+    return IconButton(
+      icon: Icon(
+        Icons.fast_forward_rounded,
+        size: 40,
+        color: Colors.green,
+      ),
+      onPressed: () {
+        audioPlayer?.setPlaybackRate(1.5);
+      },
+    );
+  }
+
+  //fastforward button
+  Widget buttonSlow() {
+    return IconButton(
+      icon: Icon(
+        Icons.fast_rewind_rounded,
+        size: 40,
+        color: Colors.green,
+      ),
+      onPressed: () {
+        //audioPlayer?.setPlaybackRate(1);
+        audioPlayer?.setPlaybackRate(0.5);
+      },
+    );
+  }
+
+  Widget buttonLoop() {
+    return IconButton(
+      icon: Icon(
+        Icons.shuffle,
+        size: 40,
+        color: Colors.green,
+      ),
+      onPressed: () {
+        if (isRepeat == false) {}
+      },
+    );
+  }
+
+  Widget buttonRepeat() {
+    return IconButton(
+      icon: Icon(
+        Icons.loop,
+        size: 40,
+        color: color,
+      ),
+      onPressed: () {
+        if (isRepeat == false) {
+          audioPlayer?.setReleaseMode(ReleaseMode.LOOP);
+          setState(
+            () {
+              color = Colors.black;
+              isRepeat = true;
+            },
+          );
+        } else if (isRepeat == true) {
+          audioPlayer?.setReleaseMode(ReleaseMode.RELEASE);
+          setState(
+            () {
+              color = Colors.green;
+              isRepeat = false;
+            },
+          );
+        }
+      },
+    );
   }
 
   @override
@@ -111,20 +236,52 @@ class _MusicPlayerState extends State<MusicPlayer> {
                     ),
                   ),
                   SizedBox(
-                    height: 40,
+                    height: 20,
                   ),
-                  IconButton(
-                    icon: Icon(
-                      audioPlayerState == PlayerState.PLAYING
-                          ? Icons.pause_circle_filled_rounded
-                          : Icons.play_circle_fill_rounded,
-                    ),
-                    iconSize: 60,
-                    onPressed: () {
-                      audioPlayerState == PlayerState.PLAYING
-                          ? pauseMusic()
-                          : playMusic();
-                    },
+
+                  slider(),
+                  // SizedBox(
+                  //   height: 15,
+                  // ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        _position.toString().split('.')[0],
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        _duration.toString().split('.')[0],
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      buttonRepeat(),
+                      buttonSlow(),
+                      IconButton(
+                        icon: Icon(
+                          audioPlayerState == PlayerState.PLAYING
+                              ? Icons.pause_circle_filled_rounded
+                              : Icons.play_circle_filled_rounded,
+                        ),
+                        iconSize: 60,
+                        color: Colors.green,
+                        onPressed: () {
+                          audioPlayerState == PlayerState.PLAYING
+                              ? pauseMusic()
+                              : playMusic();
+                        },
+                      ),
+                      buttonFast(),
+                      buttonLoop(),
+                    ],
                   ),
                   //AudioFile(advancedPlayer: advancedPlayer),
                 ],
